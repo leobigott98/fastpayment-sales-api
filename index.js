616f6cb1282bd7f0a1c6ded86aef7abb5f19aea9
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2024-02-21 16:47:17 -0400

    setting up routes

--- index.js
+++ index.js
@@ -14,8 +14,34 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 //import routes
+const signInRouter = require("./auth/sign-in.router");
+const signUpRouter = require("./auth/sign-up.router");
+const customersRouter = require("./customers/customers.router");
+const codLocalIdRouter = require("./customers/codlocalid.router");
+const addressRouter = require("./customers/address.router");
+const inventoryRouter = require("./inventory/inventory.router");
+const countryRouter = require("./customers/countries.router");
+const stateRouter = require("./customers/states.router");
+const municipalityRouter = require("./customers/municipalities.router");
+const cityRouter = require("./customers/cities.router");
+const parishRouter = require("./customers/parish.router");
+const actividyRouter = require("./customers/activity.router");
+const banksRouter = require("./customers/banks.router");
 
 //routes
+app.use('/api/v1/auth/sign-in', signInRouter);
+app.use('/api/v1/auth/sign-up', signUpRouter);
+app.use('/api/v1/customers', customersRouter);
+app.use('/api/v1/new-inventory', inventoryRouter);
+app.use('/api/v1/get-codlocalid', codLocalIdRouter);
+app.use('/api/v1/listar-paises', countryRouter);
+app.use('/api/v1/listar-estados', stateRouter);
+app.use('/api/v1/listar-municipios', municipalityRouter);
+app.use('/api/v1/listar-ciudades', cityRouter);
+app.use('/api/v1/listar-parroquias', parishRouter);
+app.use('/api/v1/new-address', addressRouter);
+app.use('/api/v1/listar-actividades', actividyRouter);
+app.use('/api/v1/listar-bancos', banksRouter);
 
 
 const port = process.env.PORT || 3001;
