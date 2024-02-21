cb2970260027f2a93fe006748d9e190828e27b5d
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2024-02-21 16:40:13 -0400

    setting up cities router

---
+++ customers/cities.router.js
@@ -0,0 +1,37 @@
+//import dependencies
+const express = require("express");
+const pool = require("../db");
+
+//import middlewares
+const auth = require("../middlewares/auth");
+
+//setup the router for express
+const router = express.Router();
+
+//Set up the route handlers
+
+
+//Get all customers
+router.post("/", [auth], async (req, res)=>{
+    const promisePool = pool.promise();
+
+    const {pais_id, estado_id} = req.body
+
+    try {
+        const result = await promisePool.query('CALL sp_get_city(?, ?)', [pais_id, estado_id]);
+
+        res.status(200).json({
+            ok: true,
+            result: result[0][0]
+        });
+        
+    } catch (error) {
+        res.status(500).send({
+            ok: false, 
+            result: 'Not sure what happened :('
+        })
+    }
+    
+});
+
+module.exports = router;
