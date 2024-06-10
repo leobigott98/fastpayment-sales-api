726da41b0422bdb943fa93f88a42166b0af715e2
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2024-06-10 13:40:24 -0400

    implemented updateTranredCustomer

--- tranred/tranred.router.js
+++ tranred/tranred.router.js
@@ -7,7 +7,7 @@ const pool = require("../db")
 //import middlewares
 const auth = require("../middlewares/auth");
 const {admin, sales, finance, sales_finance} = require("../middlewares/roles");
-const {createCustomer, getOneCustomer, getAllCustomers, editCustomer, createTerminal, getTerminal, createTranredCustomer, updatePlans, getPlans,createTerminalInDB, getCuotas, cancelCuota, getTerminalHistory, changeTerminalStatus, updateBankAccount} = require("./controllers");
+const {createCustomer, getOneCustomer, getAllCustomers, editCustomer, createTerminal, getTerminal, createTranredCustomer, updatePlans, getPlans,createTerminalInDB, getCuotas, cancelCuota, getTerminalHistory, changeTerminalStatus, updateBankAccount, updateTranredCustomer} = require("./controllers");
 
 //set up the express server router
 const router = express.Router();
@@ -105,6 +105,8 @@ router.get('/terminal/plans/all', [auth, sales_finance], getPlans)
 router.get('/terminal/getCuotasPendientes/:terminal/:planId/:startDate/:endDate', [auth, sales_finance], getCuotas)
 
 router.post('terminal/anularCuotas', [auth, sales_finance], cancelCuota)
+
+router.put('/customer', [auth, sales], updateTranredCustomer)
 
 
 //export the router
